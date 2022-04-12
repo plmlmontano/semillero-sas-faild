98852ced87421ae7aa37f7d9fa576bfae7d3a5d2
Lina Maria Montaño Ramirez <lmontano@plm.com.co>
2022-04-12 17:51:37 -0500

    cambios en contador de vehiculos

--- backend/controllers/vehiculo.controllers.js
+++ backend/controllers/vehiculo.controllers.js
@@ -91,3 +91,20 @@ export const all = async (req, res) => {
     }
 
 };
+
+export const count = async (req, res) => {
+    try {
+        const connection = await connect();
+        const [results] = await connection.query("SELECT count(*) FROM vehiculos");
+        if (![results]) {
+            return res.status(500).send("No se encontro ningun registro");
+        } else {
+            return res.status(200).json(results);
+        }
+
+    } catch (error) {
+        console.error({ error });
+        return res.status(500).json({ errorMessage: error.message })
+    }
+
+};
